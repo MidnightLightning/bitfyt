8959a98dae8e653fbaff5ff223c521cc43ecea9e
Brooks Boyd <boydb@midnightdesign.ws>
2013-04-29 07:00:46 -0500

    Add socket and bitcoin server modules

--- back_end/app.js
+++ back_end/app.js
@@ -1,6 +1,28 @@
+var io = require('socket.io');
 var connect = require('connect');
-var app = connect()
-  .use(function(req, res) {
-    res.end('Hello Connect!');
-  })
-  .listen(process.env.VMC_APP_PORT || 1337);
+var app = connect();
+var server = app.listen(process.env.VMC_APP_PORT || 1337);
+io = io.listen(server);
+
+var Bitcoin = require('bitcoinjs');
+node = new Bitcoin.Node();
+node.start();
+var chain = node.getBlockChain();
+chain.addListener('txSave', function(e) {
+  console.log(e);
+});
+
+app.use(function(req, res) {
+  res.end('Hello Index');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { result: 'Connection succeeded' });
+});
+
+/*
+var testEmit = function() {
+  io.sockets.emit('message', {now: new Date()});
+}
+setInterval(testEmit, 10000);
+*/
